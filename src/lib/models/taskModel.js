a1c0ea64726bba49096ad94746617b7b24452ba1
Tomasz <tomgaek@gmail.com>
2024-01-01 19:52:32 +0000

    remove unnecessary methods from taskModel

--- src/lib/models/taskModel.js
+++ src/lib/models/taskModel.js
@@ -3,23 +3,11 @@ import AbstractModel from '../Abstracts/model';
 class TaskModel extends AbstractModel {
     constructor() {
         super();
-        // this.properties.name = '';
-        // this.properties.id = null;
     }
 
-    setName(name) {
-        this.properties.name = name;
-    }
-
-    remove() {
-        this.fireEvent('removed');
-    }
-
-    static fromString(/* name, id */key, value) {
+    static fromString(key, value) {
         const task = new TaskModel();
         task.set(key, value);
-        // task.set('name',name);
-        // task.set('id', id);
         return task;
     }
 }
