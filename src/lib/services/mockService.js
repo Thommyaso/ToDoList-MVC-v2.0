28d8c7e98f2b41e2bddb307ebe725d2e2172a7f9
Tomasz <tomgaek@gmail.com>
2024-01-18 18:01:22 +0000

    add mockService

---
+++ src/lib/services/mockService.js
@@ -0,0 +1,42 @@
+import {v4} from 'uuid';
+import exampleTasks from '../../exampleTasks.json';
+
+class Service {
+    constructor() {
+        this.tasks = [...exampleTasks];
+    }
+
+    async getTasks() {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve({data: this.tasks});
+            }, 1500);
+        });
+    }
+
+    postTask(task) {
+        const createdTask = {
+            task,
+            id: v4(),
+        };
+
+        this.tasks.push(createdTask);
+        return new Promise((resolve) => {
+            resolve({data: {createdTask}});
+        });
+    }
+
+    deleteTask(id) {
+        return new Promise((resolve) => {
+            this.tasks = this.tasks.filter((obj) => obj.id !== id);
+            resolve({
+                data: {
+                    tasks: this.tasks,
+                    status: 'ok',
+                },
+            });
+        });
+    }
+}
+
+export default Service;
